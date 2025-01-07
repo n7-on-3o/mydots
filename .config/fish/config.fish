2cf5b2ea3965c7b7965de5a79743f7b4e6e44147
Antonio Faldetta <program_pandas_0o@icloud.com>
2025-01-07 13:25:17 +0000

    fedora bits

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -14,6 +14,12 @@ nvm use default --silent
 alias la='ls -la'
 alias vim='nvim'
 alias suv='sudo nvim'
+
+#archy
 alias pacin='sudo pacman -S'
 alias pacrm='sudo pacman -Rsn'
 alias pacup='sudo pacman -Syu'
+
+#fedora
+alias updown='sudo dnf offline-upgrade download'
+alias upboot='sudo dnf offline-upgrade reboot'
